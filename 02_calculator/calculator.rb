9bf25d284ae2f926c21859043801b78d09bee95c
Utku Ege Tuluk <utkuegetuluk@hotmail.com>
2016-08-08 19:16:34 +0300

    add calc

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,20 @@
 #write your code here
+def add(number_one, number_two)
+  number_one + number_two
+end
+def subtract(number_one, number_two)
+  number_one - number_two
+end
+def sum(array)
+  sum = 0
+  array.each do |number|
+    sum = sum + number
+  end
+  sum
+end
+def multiply(number_one,number_two)
+  number_one * number_two
+end
+def power(number_one, number_two)
+  number_one ** number_two
+end
