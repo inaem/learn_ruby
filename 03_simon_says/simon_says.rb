36c66aec121b773a57aaa46c75c0300fced21a09
Utku Ege Tuluk <utkuegetuluk@hotmail.com>
2016-08-08 19:46:57 +0300

    Simon says needs work

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,24 @@
 #write your code here
+def echo(word)
+  "#{word}"
+end
+def shout(word)
+  word = word.upcase
+  "#{word}"
+end
+
+def repeat(word, times = 2)
+  say = ("#{word}" + " ") * times
+  say[0..-2]
+end
+
+def start_of_word(word, num = 1)
+  num = num -1
+  word[0..num]
+end
+def first_word(word)
+  word.split.first
+end
+def titleize(word)
+  word.split.map(&:capitalize)*' '
+end
